0697ba736c83778db6ea89e6c549f9662b82c412
tal shachar <tal.shachar@buzzfeed.com>
2017-12-29 06:50:31 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,24 +1,4 @@
 # Add  code here!
-def prime? (prime)
-  if prime == 2
-    return
-  elsif prime == 3
-    return
-  elsif prime % 2 == 0
-    return
-  elsif prime % 3 == 0
-    return
-  else
-    i = 5
-    w = 2
-    while i * i <= prime
-      if n % i == 0
-        return FALSE
-      else
-          i += w
-          w = 6 - w
-      return TRUE
-      end
-    end
-  end
+def prime?(n)
+  (2..n/2).none?{|i| n % i == 0}
 end
